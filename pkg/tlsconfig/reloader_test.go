49625f39d4386655d4b7dc8a9304ee1f1f586fa1
Denis Issoupov <denis@ekspand.com>
2022-01-18 18:53:50 +0000

    tlsconfig package

---
+++ pkg/tlsconfig/reloader_test.go
@@ -0,0 +1,128 @@
+package tlsconfig_test
+
+import (
+	"crypto/tls"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/effective-security/porto/pkg/tlsconfig"
+	"github.com/go-phorce/dolly/testify"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func Test_KeypairReloader(t *testing.T) {
+	now := time.Now().UTC()
+	pemCert, pemKey, err := testify.MakeSelfCertRSAPem(1)
+	require.NoError(t, err)
+	require.NotNil(t, pemCert)
+	require.NotNil(t, pemKey)
+
+	pemFile := filepath.Join(os.TempDir(), "test-KeypairReloader.pem")
+	keyFile := filepath.Join(os.TempDir(), "test-KeypairReloader-key.pem")
+
+	err = ioutil.WriteFile(pemFile, pemCert, os.ModePerm)
+	require.NoError(t, err)
+	err = ioutil.WriteFile(keyFile, pemKey, os.ModePerm)
+	require.NoError(t, err)
+
+	time.Sleep(100 * time.Millisecond)
+
+	k, err := tlsconfig.NewKeypairReloader(pemFile, keyFile, 100*time.Millisecond)
+	require.NoError(t, err)
+	require.NotNil(t, k)
+	defer k.Close()
+
+	reloadedCount := 0
+	k.OnReload(func(_ *tls.Certificate) {
+		reloadedCount++
+	})
+
+	loadedAt := k.LoadedAt()
+	assert.True(t, loadedAt.After(now), "loaded time must be after test start time")
+	assert.Equal(t, uint32(1), k.LoadedCount())
+
+	err = ioutil.WriteFile(pemFile, pemCert, os.ModePerm)
+	require.NoError(t, err)
+	err = ioutil.WriteFile(keyFile, pemKey, os.ModePerm)
+	require.NoError(t, err)
+	err = ioutil.WriteFile(pemFile, pemCert, os.ModePerm)
+	require.NoError(t, err)
+
+	time.Sleep(200 * time.Millisecond)
+
+	loadedAt2 := k.LoadedAt()
+	count := int(k.LoadedCount())
+	assert.True(t, count >= 2 && count <= 4, "must be loaded at start, whithin period and after, loaded: %d", k.LoadedCount())
+	assert.True(t, loadedAt2.After(loadedAt), "re-loaded time must be after last loaded time")
+
+	err = ioutil.WriteFile(pemFile, pemCert, os.ModePerm)
+	require.NoError(t, err)
+	err = ioutil.WriteFile(keyFile, pemKey, os.ModePerm)
+	require.NoError(t, err)
+	time.Sleep(200 * time.Millisecond)
+
+	err = ioutil.WriteFile(pemFile, pemCert, os.ModePerm)
+	require.NoError(t, err)
+	err = ioutil.WriteFile(keyFile, pemKey, os.ModePerm)
+	require.NoError(t, err)
+
+	time.Sleep(200 * time.Millisecond)
+
+	loadedAt3 := k.LoadedAt()
+	count = int(k.LoadedCount())
+	assert.True(t, count >= 3 && count <= 5, "must be loaded at start, whithin period and after, loaded: %d", k.LoadedCount())
+	assert.True(t, loadedAt3.After(loadedAt2), "re-loaded time must be after last loaded time")
+	assert.True(t, reloadedCount > 1, "must be reloaded when file modified: %d", reloadedCount)
+
+	getKeypair := k.GetKeypairFunc()
+	kpair, err := getKeypair(nil)
+	require.NoError(t, err)
+	require.NotNil(t, kpair)
+
+	getClientCertificate := k.GetClientCertificateFunc()
+	kpair, err = getClientCertificate(nil)
+	require.NoError(t, err)
+	require.NotNil(t, kpair)
+}
+
+func Test_KeypairReloader_Reload(t *testing.T) {
+	pemCert, pemKey, err := testify.MakeSelfCertRSAPem(1)
+	require.NoError(t, err)
+	require.NotNil(t, pemCert)
+	require.NotNil(t, pemKey)
+
+	pemFile := filepath.Join(os.TempDir(), "test-KeypairReloader2.pem")
+	keyFile := filepath.Join(os.TempDir(), "test-KeypairReloader2-key.pem")
+
+	err = ioutil.WriteFile(pemFile, pemCert, os.ModePerm)
+	require.NoError(t, err)
+	err = ioutil.WriteFile(keyFile, pemKey, os.ModePerm)
+	require.NoError(t, err)
+
+	k, err := tlsconfig.NewKeypairReloader(pemFile, keyFile, 100*time.Millisecond)
+	require.NoError(t, err)
+	require.NotNil(t, k)
+	defer k.Close()
+
+	reloadedCount := 0
+	k.OnReload(func(_ *tls.Certificate) {
+		reloadedCount++
+	})
+
+	var wg sync.WaitGroup
+
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			k.Reload()
+		}()
+	}
+	wg.Wait()
+	assert.Equal(t, 0, reloadedCount)
+}
---
+++ pkg/tlsconfig/tlsconfig_test.go
@@ -0,0 +1,113 @@
+package tlsconfig_test
+
+import (
+	"crypto/tls"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"path/filepath"
+	"testing"
+	"time"
+
+	"github.com/effective-security/porto/pkg/tlsconfig"
+	"github.com/go-phorce/dolly/testify"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func Test_BuildFromFiles(t *testing.T) {
+	pemCert, pemKey, err := testify.MakeSelfCertRSAPem(1)
+	require.NoError(t, err)
+	require.NotNil(t, pemCert)
+	require.NotNil(t, pemKey)
+
+	tmpDir := filepath.Join(os.TempDir(), "tests", "tlsconfig")
+	err = os.MkdirAll(tmpDir, os.ModePerm)
+	require.NoError(t, err)
+
+	pemFile := filepath.Join(tmpDir, "BuildFromFiles.pem")
+	keyFile := filepath.Join(tmpDir, "BuildFromFiles-key.pem")
+
+	err = ioutil.WriteFile(pemFile, pemCert, os.ModePerm)
+	require.NoError(t, err)
+	err = ioutil.WriteFile(keyFile, pemKey, os.ModePerm)
+	require.NoError(t, err)
+
+	cfg, err := tlsconfig.NewServerTLSFromFiles(pemFile, keyFile, "", tls.RequireAndVerifyClientCert)
+	assert.NoError(t, err)
+	require.NotNil(t, cfg)
+	assert.Equal(t, tls.RequireAndVerifyClientCert, cfg.ClientAuth)
+
+	cfg, err = tlsconfig.NewServerTLSFromFiles(pemFile, keyFile, pemFile, tls.RequireAndVerifyClientCert)
+	assert.NoError(t, err)
+	require.NotNil(t, cfg)
+	assert.Equal(t, tls.RequireAndVerifyClientCert, cfg.ClientAuth)
+
+	cfg, reloader, err := tlsconfig.NewClientTLSWithReloader(pemFile, keyFile, pemFile, 5*time.Second)
+	assert.NoError(t, err)
+	require.NotNil(t, cfg)
+	assert.Equal(t, tls.NoClientCert, cfg.ClientAuth)
+	require.NotNil(t, reloader)
+	assert.NotNil(t, reloader.Keypair())
+	reloader.Close()
+
+	c, k := reloader.CertAndKeyFiles()
+	assert.Equal(t, pemFile, c)
+	assert.Equal(t, keyFile, k)
+}
+
+func Test_RoundTripper(t *testing.T) {
+	pemCert, pemKey, err := testify.MakeSelfCertRSAPem(1)
+	require.NoError(t, err)
+	require.NotNil(t, pemCert)
+	require.NotNil(t, pemKey)
+
+	pemFile := filepath.Join(os.TempDir(), "test-RoundTripper.pem")
+	keyFile := filepath.Join(os.TempDir(), "test-RoundTripper-key.pem")
+
+	err = ioutil.WriteFile(pemFile, pemCert, os.ModePerm)
+	require.NoError(t, err)
+	err = ioutil.WriteFile(keyFile, pemKey, os.ModePerm)
+	require.NoError(t, err)
+
+	h := makeTestHandler(t, "/v1/test", `{}`)
+	server := httptest.NewServer(h)
+	defer server.Close()
+
+	tr, err := tlsconfig.NewHTTPTransportWithReloader(
+		pemFile,
+		keyFile,
+		"",
+		50*time.Millisecond,
+		nil,
+	)
+	require.NoError(t, err)
+
+	time.Sleep(400 * time.Microsecond)
+
+	for i := 0; i < 5; i++ {
+		// modify files
+		err = ioutil.WriteFile(pemFile, pemCert, os.ModePerm)
+		require.NoError(t, err)
+
+		time.Sleep(400 * time.Microsecond)
+
+		r, err := http.NewRequest(http.MethodGet, "/v1/test", nil)
+		require.NoError(t, err)
+
+		_, err = tr.RoundTrip(r)
+		assert.Error(t, err)
+	}
+	tr.Close()
+}
+
+func makeTestHandler(t *testing.T, expURI, responseBody string) http.Handler {
+	h := func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, expURI, r.RequestURI, "received wrong URI")
+		w.WriteHeader(http.StatusOK)
+		io.WriteString(w, responseBody)
+	}
+	return http.HandlerFunc(h)
+}
