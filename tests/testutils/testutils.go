e618a40f66b045a795e862df2a7ace0222bb0b10
Denis Issoupov <denis@ekspand.com>
2022-01-25 15:11:11 +0000

    fixed test server ports

--- tests/testutils/testutils.go
+++ tests/testutils/testutils.go
@@ -11,8 +11,14 @@ var (
 	nextPort = int32(os.Getpid()%10000) + int32(17891) + rand.Int31n(1000)
 )
 
-// CreateURLs returns URL with a random port
-func CreateURLs(scheme, host string) string {
+// CreateURL returns URL with a random port
+func CreateURL(scheme, host string) string {
 	next := atomic.AddInt32(&nextPort, 1)
 	return fmt.Sprintf("%s://%s:%d", scheme, host, next)
+}
+
+// CreateBindAddr returns a bind address with a random port
+func CreateBindAddr(host string) string {
+	next := atomic.AddInt32(&nextPort, 1)
+	return fmt.Sprintf("%s:%d", host, next)
 }
