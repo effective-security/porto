d72e304f8b02b7a084c5df822feccf08447a131c
Denis Issoupov <denis@ekspand.com>
2022-12-18 20:40:27 +0000

    Add metrics type consts

--- metricskey/describe.go
+++ metricskey/describe.go
@@ -6,26 +6,26 @@ import "github.com/effective-security/metrics"
 var (
 	HTTPReqPerf = metrics.Describe{
 		Name:         "http_requests_perf",
-		Type:         "summary",
+		Type:         metrics.TypeSample,
 		RequiredTags: []string{"method", "status", "uri"},
 		Help:         "http_requests_perf provides quantiles for HTTP request.",
 	}
 	HTTPReqByRole = metrics.Describe{
 		Name:         "http_requests_role",
-		Type:         "counter",
+		Type:         metrics.TypeCounter,
 		RequiredTags: []string{"method", "status", "uri", "role"},
 		Help:         "http_requests_role provides counts for HTTP request by role.",
 	}
 
 	GRPCReqPerf = metrics.Describe{
 		Name:         "rpc_requests_perf",
-		Type:         "summary",
+		Type:         metrics.TypeSample,
 		RequiredTags: []string{"method", "status"},
 		Help:         "rpc_requests_perf provides quantiles for gRPC request.",
 	}
 	GRPCReqByRole = metrics.Describe{
 		Name:         "rpc_requests_role",
-		Type:         "counter",
+		Type:         metrics.TypeCounter,
 		RequiredTags: []string{"method", "status", "role"},
 		Help:         "rpc_requests_role provides counts for gRPC request by role.",
 	}
