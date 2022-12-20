12e054c1be1c8c2d449c1d755062d73dc5d7ff2e
Denis Issoupov <denis@ekspand.com>
2022-12-20 18:51:19 +0000

    updated metrics help

--- metricskey/describe.go
+++ metricskey/describe.go
@@ -8,26 +8,26 @@ var (
 		Name:         "http_requests_perf",
 		Type:         metrics.TypeSample,
 		RequiredTags: []string{"method", "status", "uri"},
-		Help:         "http_requests_perf provides quantiles for HTTP request.",
+		Help:         "provides quantiles for HTTP request.",
 	}
 	HTTPReqByRole = metrics.Describe{
 		Name:         "http_requests_role",
 		Type:         metrics.TypeCounter,
 		RequiredTags: []string{"method", "status", "uri", "role"},
-		Help:         "http_requests_role provides counts for HTTP request by role.",
+		Help:         "provides counts for HTTP request by role.",
 	}
 
 	GRPCReqPerf = metrics.Describe{
 		Name:         "rpc_requests_perf",
 		Type:         metrics.TypeSample,
 		RequiredTags: []string{"method", "status"},
-		Help:         "rpc_requests_perf provides quantiles for gRPC request.",
+		Help:         "provides quantiles for gRPC request.",
 	}
 	GRPCReqByRole = metrics.Describe{
 		Name:         "rpc_requests_role",
 		Type:         metrics.TypeCounter,
 		RequiredTags: []string{"method", "status", "role"},
-		Help:         "rpc_requests_role provides counts for gRPC request by role.",
+		Help:         "provides counts for gRPC request by role.",
 	}
 )
 
